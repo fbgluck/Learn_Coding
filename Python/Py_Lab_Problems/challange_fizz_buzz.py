3d5727ffcc5c9a6db12af656af36e7214655d8f6
Fredric Gluck <fbgluck@gmail.com>
2024-07-17 13:57:02 -0400

    added fizz_Buzz.py from replit

---
+++ Python/Py_Lab_Problems/challange_fizz_buzz.py
@@ -0,0 +1,16 @@
+## Fizz Buzz Test - 
+loop_counter = 0
+while loop_counter <= 99:
+  loop_counter += 1
+  if loop_counter % 15 == 0:
+      print("fizzBuzz")
+      continue
+  elif loop_counter % 3 == 0:
+      print("fizz")
+      continue
+  elif loop_counter % 5 == 0:
+      print("buzz")
+      continue
+  else:
+    print(loop_counter)
+
