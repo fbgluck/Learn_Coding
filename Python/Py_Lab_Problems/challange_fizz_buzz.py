60202311d025d04e73cc9b6746afa29ca38e3f5f
Fredric Gluck <fbgluck@gmail.com>
2024-10-17 14:27:46 -0400

    Slight changes to output on Fizz buzz

--- Python/Py_Lab_Problems/challange_fizz_buzz.py
+++ Python/Py_Lab_Problems/challange_fizz_buzz.py
@@ -3,15 +3,15 @@ loop_counter = 0
 while loop_counter <= 99:
   loop_counter += 1
   if loop_counter % 15 == 0:
-      print("fizzBuzz")
+      print(f"{loop_counter} : fizzBuzz (5 and 3)")
       continue
   elif loop_counter % 3 == 0:
-      print("fizz")
+      print(f"{loop_counter} : fizz (3)")
       continue
   elif loop_counter % 5 == 0:
-      print("buzz")
+      print(f"{loop_counter} : buzz(5)")
       continue
   else:
-    print(loop_counter)
+    print(f"{loop_counter} : neither fizz nor buzz")
 
 
