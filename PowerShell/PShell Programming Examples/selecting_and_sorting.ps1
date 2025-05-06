21bd2c253b9d8e6e1dc70563e09cbf1abc5c00a2
Fredric Gluck <fbgluck@gmail.com>
2025-05-06 10:15:27 -0400

    added system inventory powershell

--- PowerShell/PShell Programming Examples/selecting_and_sorting.ps1
+++ PowerShell/PShell Programming Examples/selecting_and_sorting.ps1
@@ -1,3 +1,3 @@
 # Example program that shows retrieving data and sorting and displaying top n results
 get-process | select-object ProcessName, StartTime,CPU,TotalProcessorTime -first 5 |sort-object -property TotalProcessorTime -descending
-
+write-host "hello world" # this is a comment a the end of a line
---
+++ PowerShell/PShell Programming Examples/system_inventory.ps1
@@ -0,0 +1,1 @@
+Get-ComputerInfo | Select-Object "BiosReleaseDate", "LogonServer", "CSModel"
