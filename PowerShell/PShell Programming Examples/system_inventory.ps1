d3512ce7fb3c8d7c0db5c07e284cd682a30e44a2
Fredric B. Gluck <fbgluck@gmail.com>
2025-05-08 19:41:00 -0400

    Changes to System Inventory

--- PowerShell/PShell Programming Examples/system_inventory.ps1
+++ PowerShell/PShell Programming Examples/system_inventory.ps1
@@ -1,1 +1,27 @@
-Get-ComputerInfo | Select-Object "BiosReleaseDate", "LogonServer", "CSModel"
+# Get-ComputerInfo | Select-Object "BiosReleaseDate", "LogonServer", "CSModel"
+Clear-Host
+# Initialize Counting Variables
+$enUsers = 0
+$disUsers = 0
+Write-Host ("Local User Status for: $env:COMPUTERNAME") # contents of environment variable
+Write-Host "----------------------------------"
+
+Get-LocalUser | 
+ForEach-Object -Process {
+    write-host ("Local user: $($PSItem.Name)") -NoNewline # For the current item in the for each
+    if ($PSItem.Enabled) {      #.Enabled is a boolean value
+        write-host (" is Enabled") -ForegroundColor "Red"
+        $enUsers=$enUsers+1
+    }
+    else {
+        write-host (" is Disabled") -ForegroundColor "Green"
+        $disUsers = $disUsers +1
+    }
+} # end of Process Loop
+Write-Host "--------------------------------------"
+Write-Host ("Total of $enUsers enabled users")
+Write-Host ("Total of $disUsers disabled users")
+Write-Host
+
+$cpuInfo = Get-CimInstance -ClassName Win32_Processor
+write-Host$cpuInfo.Name -contains "Intel(R) Xeon(R) Gold 5118 CPU @ 2.30GHz" # should be True
